6d56137c1361399f2c09cffd5e4487d27c390358
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-04-19 02:54:15 -0700

    chore: add Separator component and update dependencies

---
+++ packages/ui/src/components/Separator.tsx
@@ -0,0 +1,27 @@
+import {
+  Separator as AriaSeparator,
+  type SeparatorProps,
+} from "react-aria-components/Separator";
+import { tv } from "tailwind-variants";
+
+const separatorVariants = tv({
+  base: "border-none bg-border forced-colors:bg-[ButtonBorder]",
+  variants: {
+    orientation: {
+      horizontal: "h-px w-full",
+      vertical: "h-full min-h-8 w-px",
+    },
+  },
+  defaultVariants: {
+    orientation: "horizontal",
+  },
+});
+
+const Separator = ({ className, orientation, ...props }: SeparatorProps) => (
+  <AriaSeparator
+    className={separatorVariants({ className, orientation })}
+    {...props}
+  />
+);
+
+export { Separator, type SeparatorProps };
