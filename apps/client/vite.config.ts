ecb251b1e50e8f1cd03a8bbce03259cef0c976da
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-05 15:34:52 -0700

    feat: add Lexend font and webfont download plugin
    - Remove @source "src/"; from globals.css since not necessary
    - Set default font to Lexend
    - Add Vite webfont download plugin
    - Add Lexend from Google Fonts to index.html

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,12 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { Features } from "lightningcss";
 import { defineConfig } from "vite";
+import { viteWebfontDownload } from "vite-plugin-webfont-dl";
 
 const viteConfig = defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    viteWebfontDownload(),
   ],
   css: {
     transformer: "lightningcss",
