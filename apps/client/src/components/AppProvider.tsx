07c6f28fb8899054b712eaa54e8f3aee2e4cbe92
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-15 00:22:42 -0700

    refactor: remove RouterProvider component

--- apps/client/src/components/AppProvider.tsx
+++ apps/client/src/components/AppProvider.tsx
@@ -4,16 +4,12 @@ import { ThemeProvider } from "next-themes";
 
 import { ToastRegion } from "@ui/ui/components/Toast";
 
-import { RouterProvider } from "./RouterProvider";
-
 const AppProvider = ({ children }: { children: Readonly<ReactNode> }) => {
   return (
-    <RouterProvider>
-      <ThemeProvider>
-        {children}
-        <ToastRegion />
-      </ThemeProvider>
-    </RouterProvider>
+    <ThemeProvider>
+      {children}
+      <ToastRegion />
+    </ThemeProvider>
   );
 };
 
