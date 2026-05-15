37db7031fcc5570f80f8c5dda94efc28f2657cd9
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-05-15 00:32:58 -0700

    fix: update composeTailwindRenderProps to handle renderProps for tw argument

--- packages/ui/src/utils/composeTailwindRenderProps.ts
+++ packages/ui/src/utils/composeTailwindRenderProps.ts
@@ -3,9 +3,11 @@ import { twMerge, type ClassNameValue } from "tailwind-merge";
 
 const composeTailwindRenderProps = <T>(
   className: string | ((renderProps: T) => string) | undefined,
-  tw: ClassNameValue,
+  tw: ClassNameValue | ((renderProps: T) => ClassNameValue),
 ): string | ((v: T) => string) => {
-  return composeRenderProps(className, (className) => twMerge(tw, className));
+  return composeRenderProps(className, (className, renderProps) =>
+    twMerge(typeof tw === "function" ? tw(renderProps) : tw, className),
+  );
 };
 
 export { composeTailwindRenderProps };
